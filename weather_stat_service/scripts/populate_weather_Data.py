6679a29a43867bfeeefcc479fc7681b973fc5194
vsarode <vitthal.sarode1@gmail.com>
2018-01-20 14:09:05 +0000

    population script updated

--- weather_stat_service/scripts/populate_weather_Data.py
+++ weather_stat_service/scripts/populate_weather_Data.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib2
-import re
+import django
+
+django.setup()
+
+from weather_stat_service.db.weather_stat.models import WeatherInfo, Month
 
 hdr = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -23,9 +27,30 @@ def get_the_file_urls():
     return file_links
 
 
+def make_the_db_entries(country, factor, year, data):
+    weather_info_object = WeatherInfo.objects.create(country=country,
+                                                     year=year,
+                                                     factor=factor)
+    month_values_objects = weather_info_object.month_values.all().order_by('id')
+    for index, month in enumerate(month_values_objects):
+        month.value = data[index]
+        month.save()
+    print "entry made for ", country, factor, year
+
+
 def populate_data(file_links):
     for url in file_links:
-        response = urllib2.urlopen(url)
-        html = response.read()
-        print html
-    
+        req = urllib2.Request(url, headers=hdr)
+        response = list(urllib2.urlopen(req))[8:]
+        country = url.split('/')[-1].split('.')[0]
+        factory = url.split('/')[-3]
+        for entry in response:
+            list_of_values = map(lambda x: x.lstrip().rstrip(), entry.split('   '))
+            year = list_of_values[0]
+            make_the_db_entries(country, factory, year, list_of_values[1:-2])
+
+
+if __name__ == "__main__":
+    file_links = get_the_file_urls()
+    print file_links
+    populate_data(file_links)
